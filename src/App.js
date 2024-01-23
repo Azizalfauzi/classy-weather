d50cdafe1270a0cb17b146da64ce11f416ada708
Aziz Alfa <azizalfauzi65@gmail.com>
2024-01-23 09:56:36 +0700

    Modified constructor to bind some state increment and decrement

--- src/App.js
+++ src/App.js
@@ -3,14 +3,28 @@ class Counter extends React.Component {
   constructor(props) {
     super(props);
     this.state = { count: 5 };
+    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  handleDecrement() {
+    this.setState((currState) => {
+      return { count: currState.count - 1 };
+    });
+  }
+
+  handleIncrement() {
+    this.setState((currState) => {
+      return { count: currState.count + 1 };
+    });
   }
 
   render() {
     return (
       <div>
-        <button>-</button>
+        <button onClick={this.handleDecrement}>-</button>
         <span>{this.state.count}</span>
-        <button>+</button>
+        <button onClick={this.handleIncrement}>+</button>
       </div>
     );
   }
